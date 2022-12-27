4e6253e12066ba3fce852e9fc6389955bd7637c9
gyulaidavid <gyulaidavid@gmail.com>
2022-12-27 22:01:23 +0100

    yellow star added - works

--- src/components/Artpiece.jsx
+++ src/components/Artpiece.jsx
@@ -1,22 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import "../App.css";
 
 function Artpiece({ artPieceData }) {
-
-
+  const [isFavorite, setIsFavorite] = useState(true);
 
   return (
     <div className='card'>
-      <div className="img-container">
-       <img src={artPieceData.primaryImage
-} alt={artPieceData.title} />
-</div>
+      <div className='img-container'>
+        <img src={artPieceData.primaryImage} alt={artPieceData.title} />
+      </div>
       {/* <h2>{artPieceData.title}</h2> */}
       <h1> Technique: {artPieceData.objectName}</h1>
       <h2> Title: {artPieceData.title}</h2>
       <p>Classification: {artPieceData.classification}</p>
-      
-
+      <p className="star"
+        onClick={() => {
+          setIsFavorite((oldValue) => !oldValue);
+        }}
+      >
+        {isFavorite ? "☆" : "★"}
+      </p>
     </div>
   );
 }
