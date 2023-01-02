c63782e8c8a54a3b518e040d0a8f879b661ba286
gyulaidavid <gyulaidavid@gmail.com>
2023-01-02 12:57:07 +0100

    search not working

--- src/App.js
+++ src/App.js
@@ -7,11 +7,20 @@ import Login from "./components/Login";
 import Error from "./components/Error";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-import Button from 'react-bootstrap/Button';
+import Button from "react-bootstrap/Button";
 
 function App() {
   const apiKey = "ac3cc164-cd23-4a7e-8d4e-7dd367deafb5";
+
+  //  Képek fetchelése az oldalra
   const [artpieces, setArtpieces] = useState([]);
+
+  // Keresés fetchelése
+  const [searchField, setSearchField] = useState([]);
+
+  //Keresőszó state
+  const[searchWord, setSearchWord] = useState('')
+
   const [perPage, setPerPage] = useState(10);
   const [filter, setFilter] = useState("");
   const [sortBy, setSortBy] = useState("Date asc");
@@ -27,20 +36,32 @@ function App() {
         }, 1000);
       });
   }, [perPage]);
+  
+    console.log(artpieces);
 
-  console.log(artpieces);
+//Keresőszó
+
+  useEffect(() => {
+    fetch(`https://api.harvardartmuseums.org/image?q=${searchWord}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setSearchField(data.records);
+      }, 1000);
+  }, [perPage]);
+
+  console.log(searchField);
+
 
   useEffect(() => {
     sortBy === "asc"
       ? setArtpieces([...artpieces].sort((a, b) => (a.date < b.date ? 1 : -1)))
-      : setArtpieces([...artpieces].sort((a, b) => (a.date > b.date ? 1 : -1)))
-      
+      : setArtpieces([...artpieces].sort((a, b) => (a.date > b.date ? 1 : -1)));
   }, [sortBy]);
 
   return (
     <>
       <div className='App'>
-      <p style={{ color: "red" }}>
+        <p style={{ color: "red" }}>
           Hogy lehet +/- gomb használatával itemet hozzáadni, elvenni?
         </p>
         <input
@@ -50,8 +71,8 @@ function App() {
             setPerPage(event.target.value);
           }}
         />
-            <p>Filter: </p>
-      {/* <input
+        <p>Filter: </p>
+        {/* <input
         type='text'
         placeholder='filter'
         value={filter}
@@ -60,20 +81,30 @@ function App() {
         }}
       /> */}
 
-
-<Button variant="warning"
-        onClick={() => {
-          sortBy === "Date asc" ? setSortBy("Date desc") : setSortBy("Date asc");
+<p>Search: </p>
+        <input
+        type='text'
+        placeholder='Type your search'
+        value={searchWord}
+        onChange={(event) => {
+          setSearchWord(event.target.value);
         }}
-      >
-        {" "}
-        Sort by {sortBy}{" "}
-      </Button>
+      />
 
-      
+        <Button
+          variant='warning'
+          onClick={() => {
+            sortBy === "Date asc"
+              ? setSortBy("Date desc")
+              : setSortBy("Date asc");
+          }}
+        >
+          {" "}
+          Sort by {sortBy}{" "}
+        </Button>
+
         <BrowserRouter>
           <Routes>
-
             <Route
               path='/'
               element={
